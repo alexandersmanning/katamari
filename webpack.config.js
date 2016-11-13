a68d21058353612809af421a1483f4a3d2ae517a
Alex Manning <alexmanning@Alexs-Mac-mini.local>
2016-11-12 21:02:51 -0800

    Created moving piece that bounce off the walls, and are aware of hitting on another

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  entry: "./lib/katamari.js",
+  entry: "./lib/main.js",
   output: {
   	filename: "./lib/bundle.js"
+  },
+   module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js' ]
   },
   devtool: 'source-map',
 };
